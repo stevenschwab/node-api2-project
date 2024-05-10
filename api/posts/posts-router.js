116b22abeafd5a0643021233b2479aec2a31ef7c
Steve Schwab <schwabs@umich.edu>
2024-05-10 14:31:37 -0700

    implement put call to /api/posts/:id route

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -66,4 +66,38 @@ router.post('/', (req, res) => {
         })
 });
 
+router.put('/:id', (req, res) => {
+    const { title, contents } = req.body;
+    const { id } = req.params;
+
+    if (!title || !contents) {
+        return res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    }
+    
+    Posts.update(id, { title, contents })
+        .then(count => {
+            if (count) {
+                return Posts.findById(id)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                });
+                return;
+            }
+        })
+        .then(updatedPost => {
+            if (updatedPost) {
+                res.json(updatedPost)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post information could not be modified",
+                error: err.message
+            })
+        })
+})
+
 module.exports = router
