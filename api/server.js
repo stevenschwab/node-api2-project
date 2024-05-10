8d65c3fb41c03c6d34002d8b014be8a2aa3f5668
Steve Schwab <schwabs@umich.edu>
2024-05-10 15:37:26 -0700

    install cors and import it to the server file

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express')
+const cors = require('cors')
 
 const postsRouter = require('./posts/posts-router')
 
 const server = express()
 
 server.use(express.json())
+server.use(cors())
 server.use('/api/posts', postsRouter)
 
 server.get('/', (req, res) => {
